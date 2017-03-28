4eaaf0cb33cdf9962d1f3118466d79d3a15f0286
jaga <kandros5591@gmail.com>
2017-03-28 19:21:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const Promise = require('bluebird');
+const cloudinary = require('cloudinary');
+const path = require('path')
+
+let BaseStore;
+try {
+    BaseStore = require('ghost/core/server/storage/base');
+} catch(e) {
+    if (e.code !== 'MODULE_NOT_FOUND') throw e;
+    BaseStore = require(path.join(process.cwd(), 'core/server/storage/base'));
+}
+
+class Store extends BaseStore {
+    constructor(config) {
+        super()
+        this.config = config || {}
+        cloudinary.config(config);
+    }
+    
+    save(image) {
+        const secure = this.config.secure || false;
+        
+        return new Promise(function(resolve) {
+            cloudinary.uploader.upload(image.path, function(result) {
+                resolve(secure ? result.secure_url : result.url);
+            });
+        });
+    }
+    
+    serve() {
+        return function(req, res, next) {
+            next();
+        };
+    }
+    
+    read() {
+    
+    }
+    
+    delete() {
+    
+    }
+    
+    exists() {
+    
+    }
+}
+
+module.exports = Store;
